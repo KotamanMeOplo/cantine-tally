4839b73b2230be6d659e84f22d3e01af5b0c55b0
Andreas <AndrewNaTesla@gmail.com>
2018-12-26 23:21:39 +0200

    Updates

---
+++ precache-manifest.25e0c26034d7eb5bfe8485b15236a04f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1bb85021d5220f6f725",
+    "url": "/cantine-tally/static/js/runtime~main.c1bb8502.js"
+  },
+  {
+    "revision": "5b65acafc7ec6a52b532",
+    "url": "/cantine-tally/static/js/main.5b65acaf.chunk.js"
+  },
+  {
+    "revision": "ed40390b240795e0b594",
+    "url": "/cantine-tally/static/js/1.ed40390b.chunk.js"
+  },
+  {
+    "revision": "5b65acafc7ec6a52b532",
+    "url": "/cantine-tally/static/css/main.991f0b9b.chunk.css"
+  },
+  {
+    "revision": "fe2b75b47fe7e6aa86d8b3957bfe01dd",
+    "url": "/cantine-tally/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cantine-tally/precache-manifest.25e0c26034d7eb5bfe8485b15236a04f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cantine-tally/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
